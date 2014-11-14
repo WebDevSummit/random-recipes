afca6cd46f50ed7edb03051bd51ec36a17b4e379
Eric Katz <admin@ericnkatz.com>
2014-11-13 22:31:47 -0500

    Update Index

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php include('header.php'); ?>
+
+<?php 
+	/**
+	 * config stuff
+	 * @misc vars
+	 */
+	$list = '51a2ce627358a5dd30001a02';
+	$exclude = ['Meh List', 'Baked Goods']; 
+	
+	/**
+	 * include
+	 */
+	include ('TrelloBoard.php');
+
+	$board = new TrelloBoard($list, $exclude);
+	$lists = $board->getLists();
+	$cards = $board->getCards();
+
+	echo $board->renderListNavigation();
+	echo $board->renderCards();
+
+?>
+
+<ul>
+	<li><a data-url="" class="hate" href="">I Hate Dis</a></li>
+	<li><a class="like" href="">I Like Dis</a></li>
+</ul>
+
+
+<?php include('footer.php'); ?>
