d943cef2a89604f4f55cfb42e6bbdf297a46d17a
John Hartley <johnbhartley@gmail.com>
2014-11-13 22:32:40 -0500

    commenting on plugin.js

--- js/plugin.js
+++ js/plugin.js
@@ -1,16 +1,35 @@
-$('.items a, .hate').on('click', function(e) {
-	$('.cards li').removeClass('show');
+$('.items a').on('click', function(e) {
 	e.preventDefault(); 
-	var id = $(this).data('board');
-	//console.log(id);
-	var count = $('.cards li[data-list="'+id+'"]').length;
-	//console.log(count);
-	var numRand = Math.floor(Math.random() * (count + 1));
-	//console.log(numRand);
-	var card = $('.cards li[data-list="'+id+'"]').eq(numRand);
-	card.addClass('show');
+
+	// make sure they are all hidden
+	$('.cards li').removeClass('show');
+	
+	// get the board data attribute of the clicked item
+	var board = $(this).data('board');
+
+	// if board exists then find the cards and get a random number
+	if(board) {
+		var cards = $('.cards li[data-list="'+board+'"]');
+
+		// get a random number
+		// all the cards in the specific board + 1
+		// round the number down
+		var numRand = Math.floor(Math.random() * (cards.length + 1));
+	}
+	
+	// get one of the cards in this list that is equal to 
+	// numRand - 1 is because of the eq starting at 0
+	var card = $('.cards li[data-list="'+board+'"]').eq(numRand - 1);
+
+	// get the URL of the selected card
 	var url = card.data('url');
-	//console.log(url);
+
+	// show that beautiful card
+	card.addClass('show');
+	
+	// toss the card's URL into the "I Like Dis" button so user can hop straight to card
 	$('a.like').attr('href', url);
-	$('a.hate').data('board', id);
+
+	// give the hate button the board ID, so if they say they hate it they can get a new card from the same board
+	$('a.hate').data('board', board);
 });
